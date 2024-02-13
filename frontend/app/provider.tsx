3a8f424d77078996d2c7dcb4f070e6cc3409d298
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2024-02-14 00:00:15 +0530

    feat: gas Fee tank added

--- frontend/app/provider.tsx
+++ frontend/app/provider.tsx
@@ -3,21 +3,13 @@
 import "@rainbow-me/rainbowkit/styles.css";
 import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { WagmiProvider } from "wagmi";
-<<<<<<< HEAD
 import { baseSepolia, base } from "wagmi/chains";
-=======
-import { baseGoerli, base, baseSepolia } from "wagmi/chains";
->>>>>>> b153ae9efbf98516fef189fb184d0064fc4b707c
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
 const config = getDefaultConfig({
   appName: "My RainbowKit App",
   projectId: "afdac16b07284976cc7f71299771b2b7",
-<<<<<<< HEAD
   chains: [baseSepolia],
-=======
-  chains: [baseSepolia, baseGoerli, base],
->>>>>>> b153ae9efbf98516fef189fb184d0064fc4b707c
 });
 
 const queryClient = new QueryClient();
